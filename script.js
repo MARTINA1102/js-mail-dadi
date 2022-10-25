0dea130f1f60fc83fb932828d6e2490e9d23eee3
MARTINA1102 <martinamiccoli11@gmail.com>
2022-10-25 15:29:12 +0200

    esercizio dadi

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 */
-
+/*
 const mailList=['mariorossi@gmail.com','giorgiobiachi@gmail.com','andreamarroni@gmail.com'];
 const utenteMail = prompt('Quale è la tua email?');
 console.log(utenteMail);
@@ -12,7 +12,7 @@ if (mailList.includes(utenteMail)){
 }else{
     console.log('NON SEI PRESENTE NELLA LISTA');
 }
-
+*/
 
 
 
@@ -21,3 +21,12 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
+let numeroCasuale=parseInt(Math.floor(Math.random() * 6) + 1);
+console.log(numeroCasuale);
+let numeroUtente= parseInt(prompt('scegli un numero da 1 a 6'));
+console.log(numeroUtente);
+if (numeroCasuale>numeroUtente){
+    console.log('hai perso');
+}else{
+    console.log('hai vinto')
+}
